6c5a7da7152e133545958c3c98fa04a401308226
urkejov1996 <urkejov1996@gmail.com>
2024-09-10 20:06:00 +0200

    started adding services

--- src/main/java/com/urkejov/shopingcart/service/product/ProductService.java
+++ src/main/java/com/urkejov/shopingcart/service/product/ProductService.java
@@ -7,10 +7,24 @@ import java.util.List;
 public interface ProductService {
 
     Product addProduct(Product product);
-    Product getProductById(String productId);
-    void deleteProductById(String productId);
-    void updateProduct(Product product, String productId);
-    List<Product> getAllProducts();
-    List<Product> getProductsByCategoryId(String categoryId);
 
+    Product getProductById(String productId);
+
+    void deleteProductById(String productId);
+
+    void updateProduct(Product product, String productId);
+
+    List<Product> getAllProducts();
+
+    List<Product> getProductsByCategory(String category);
+
+    List<Product> getProductsByBrand(String brand);
+
+    List<Product> getProductsByCategoryAndBrand(String category, String brand);
+
+    List<Product> getProductsByName(String name);
+
+    List<Product> getProductsByBrandAndName(String brand, String name);
+
+    Long countProductsByBrandAndName(String brand, String Name);
 }
