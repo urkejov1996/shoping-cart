84a204a4f6bfb927bbb0a5a13dd0509974c97014
urkejov1996 <urkejov1996@gmail.com>
2024-09-13 16:06:42 +0200

    adding category service implementation

--- src/main/java/com/urkejov/shopingcart/service/category/CategoryService.java
+++ src/main/java/com/urkejov/shopingcart/service/category/CategoryService.java
@@ -1,4 +1,15 @@
 package com.urkejov.shopingcart.service.category;
 
+import com.urkejov.shopingcart.model.Category;
+
+import java.util.List;
+
 public interface CategoryService {
+
+    Category getCategoryById(String categoryId);
+    Category getCategoryByName(String name);
+    List<Category> getAllCategories();
+    Category addCategory(Category category);
+    Category updateCategory(Category category);
+    void deleteCategoryById(String categoryId);
 }
--- src/main/java/com/urkejov/shopingcart/service/category/CategoryServiceImpl.java
+++ src/main/java/com/urkejov/shopingcart/service/category/CategoryServiceImpl.java
@@ -1,4 +1,49 @@
 package com.urkejov.shopingcart.service.category;
 
-public class CategoryServiceImpl {
+import com.urkejov.shopingcart.exceptions.ResourceNotFoundException;
+import com.urkejov.shopingcart.model.Category;
+import com.urkejov.shopingcart.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public Category getCategoryById(String categoryId) {
+        return categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found."));
+    }
+
+    @Override
+    public Category getCategoryByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category addCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public Category updateCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public void deleteCategoryById(String categoryId) {
+        categoryRepository.findById(categoryId).ifPresentOrElse(categoryRepository::delete, () -> {
+            throw new ResourceNotFoundException("Category not found");
+        });
+    }
 }
