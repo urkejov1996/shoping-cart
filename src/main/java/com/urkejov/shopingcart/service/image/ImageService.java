95beb78195a2ec8eea74328e3a32f7ad410a43ed
urkejov1996 <urkejov1996@gmail.com>
2024-09-16 14:28:57 +0200

    adding image service implementation

--- src/main/java/com/urkejov/shopingcart/service/image/ImageService.java
+++ src/main/java/com/urkejov/shopingcart/service/image/ImageService.java
@@ -1,4 +1,15 @@
 package com.urkejov.shopingcart.service.image;
 
+import com.urkejov.shopingcart.model.Image;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface ImageService {
+
+    Image getImageById(String imageId);
+
+    void deleteImageById(String imageId);
+
+    Image saveImage(MultipartFile file, String productId);
+
+    void updateImage(MultipartFile file, String imageId);
 }
--- src/main/java/com/urkejov/shopingcart/service/image/ImageServiceImpl.java
+++ src/main/java/com/urkejov/shopingcart/service/image/ImageServiceImpl.java
@@ -1,4 +1,29 @@
 package com.urkejov.shopingcart.service.image;
 
-public class ImageServiceImpl {
+import com.urkejov.shopingcart.model.Image;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ImageServiceImpl implements ImageService {
+
+    @Override
+    public Image getImageById(String imageId) {
+        return null;
+    }
+
+    @Override
+    public void deleteImageById(String imageId) {
+
+    }
+
+    @Override
+    public Image saveImage(MultipartFile file, String productId) {
+        return null;
+    }
+
+    @Override
+    public void updateImage(MultipartFile file, String imageId) {
+
+    }
 }
