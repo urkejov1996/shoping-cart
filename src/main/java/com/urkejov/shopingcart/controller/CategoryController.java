a33341ccf8b5aa0a605faa60e7f0d0c97a76bc69
urkejov1996 <urkejov1996@gmail.com>
2024-09-18 17:23:20 +0200

    creating category controller

--- src/main/java/com/urkejov/shopingcart/controller/CategoryController.java
+++ src/main/java/com/urkejov/shopingcart/controller/CategoryController.java
@@ -1,9 +1,14 @@
 package com.urkejov.shopingcart.controller;
 
+import com.urkejov.shopingcart.service.category.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+
+    private final CategoryService categoryService;
 }
