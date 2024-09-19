5744d6132b126343eaa5e61b58622d9ec88434a7
urkejov1996 <urkejov1996@gmail.com>
2024-09-19 17:19:37 +0200

    creating category controller

--- src/main/java/com/urkejov/shopingcart/controller/CategoryController.java
+++ src/main/java/com/urkejov/shopingcart/controller/CategoryController.java
@@ -1,9 +1,17 @@
 package com.urkejov.shopingcart.controller;
 
+import com.urkejov.shopingcart.exceptions.AlreadyExistsException;
+import com.urkejov.shopingcart.model.Category;
+import com.urkejov.shopingcart.response.ApiResponse;
 import com.urkejov.shopingcart.service.category.CategoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
 
     private final CategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories() {
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Found!", categories));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error: ", INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody Category name) {
+        try {
+            Category category = categoryService.addCategory(name);
+            return ResponseEntity.ok(new ApiResponse("Success!", category));
+        } catch (AlreadyExistsException e) {
+            return ResponseEntity.status(CONFLICT).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+
 }
+
